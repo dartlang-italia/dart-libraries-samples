458beacfe51b67ba81179616038fb3235035cf2c
Claudio d'Angelis <claudiodangelis@gmail.com>
2014-07-01 19:53:02 +0200

    Finished math

--- math/mathematical_functions/bin/mathematical_functions.dart
+++ math/mathematical_functions/bin/mathematical_functions.dart
@@ -1,7 +1,15 @@
-// Imports the library
 import 'dart:math';
 
 main() {
+
+  // Max of two numbers
+  print(max(50, 60));             // 60
+  // Max of three numbers
+  print(max(max(10, 100), 1000)); // 1000
+  // Min of two numbers
+  print(min(-50, 50));            // -50
+  // Min of three numbers
+  print(min(min(1, 8), 5));       // 1
 
   // Random numbers
   // New random number generator
@@ -20,6 +28,16 @@ main() {
   // Positive square root (returns a double)
   print(sqrt(1764));  // 42.0
 
+  // Rounding numbers (methods from dart:core library)
+  // Notice that double numbers are in parentheses to provide better readability
+  print((1.34).round());        // 1
+  print((1.51).round());        // 2
+  print((1.1).floor());         // 1
+  print((1.1).floorToDouble()); // 1.0
+  print((4.2).ceil());          // 5
+  print((4.2).ceilToDouble());  // 5.0
+
+
   // Powers
   print(pow(2, 11));      // 2048
   print(pow(1764, 0.5));  // 42.0
@@ -27,18 +45,23 @@ main() {
   print(pow(3, -1));      // 0.3333333333333333
 
   // Trigonometric functions. Arguments are expressed in radians
-  print(PI);  // 3.141592653589793
-  print(sin(30 * PI/180));  // 0.49999999999999994
-  print(sin(toRadians(30)));  // 0.49999999999999994
+  print(PI);                  // 3.141592653589793
+  print(sin(30 * PI/180));    // 0.49999999999999994
+  print(cos(toRadians(30)));  // 0.8660254037844387
+  print(tan(toRadians(30)));  // 0.5773502691896257
 
-  // Asserting trigonometric identities
-  double fn1 = tan(toRadians(30));
-  print(tan(toRadians(29.999999999999994671)));
-  print(sin(toRadians(30)) * 1 / cos(toRadians(30)));
+  // Inverses
+  print(toDegrees(asin(1)));      // 90.0
+  print(toDegrees(acos(0.866)));  // 30.002910931188026
+  print(toDegrees(atan(0.577)));  // 29.984946007397852
 
-  // TODO: add more samples, fix the above assertion
-
+  // Natural logarithms and natural exponent (e)
+  print(log(10)); // 2.302585092994046
+  print(exp(1));  // 2.718281828459045
+  print(E);       // 2.7182818[...] `E` is the base of the natural logarithms
+  print(pow(E, log(42)).ceilToDouble() == log(exp(42))); // true
 }
 
-// Helper function to convert degrees to radians
+// Helper functions to convert degrees to radians and viceversa
 double toRadians(num theta) => theta * (PI/180);
+double toDegrees(num theta) => theta * (180/PI);
