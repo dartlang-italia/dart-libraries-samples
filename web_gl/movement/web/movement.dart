d57759ec8e84150382da4f7b81bb5da4613de410
Claudio d'Angelis <claudiodangelis@gmail.com>
2014-07-07 19:17:42 +0200

    Continuing webgl

--- web_gl/movement/web/movement.dart
+++ web_gl/movement/web/movement.dart
@@ -1,5 +1,6 @@
 import 'dart:html';
 import 'dart:web_gl' as web_gl;
+import 'dart:typed_data';
 
 void main() {
   // TODO: credits to http://learningwebgl.com/lessons/lesson03/index.html
@@ -10,8 +11,45 @@ void main() {
   } else {
     print("WebGL not supported");
   }
-  getShader(gl, "#shader-fs");
-  // TODO: complete
+  web_gl.Shader fragmentShader = getShader(gl, "#shader-fs");
+  web_gl.Shader vertexShader = getShader(gl, "#shader-vs");
+
+  web_gl.Program shaderProgram = gl.createProgram();
+
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+
+  gl.linkProgram(shaderProgram);
+
+  if (gl.getProgramParameter(shaderProgram, web_gl.LINK_STATUS) == null) {
+    print("Could not initialize shaders");
+  }
+
+  gl.useProgram(shaderProgram);
+
+  int aVertexPosition = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+  gl.enableVertexAttribArray(aVertexPosition);
+
+  int aVertexColor = gl.getAttribLocation(shaderProgram, "aVertexColor");
+  gl.enableVertexAttribArray(aVertexColor);
+
+  web_gl.UniformLocation pMatrixUniform = gl.getUniformLocation(shaderProgram,
+      "uPMatrix");
+  web_gl.UniformLocation mvMatrixUniform = gl.getUniformLocation(shaderProgram,
+      "uMVMatrix");
+
+  web_gl.Buffer triangleVertexPositionBuffer = gl.createBuffer();
+  gl.bindBuffer(web_gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
+  var vertices = [
+     0.0,  1.0,  0.0,
+    -1.0, -1.0,  0.0,
+     1.0, -1.0,  0.0
+     ];
+
+  gl.bufferDataTyped(web_gl.ARRAY_BUFFER, new Float32List.fromList(vertices),
+      web_gl.RenderingContext.STATIC_DRAW);
+
+  // TODO: continue
 }
 
 web_gl.Shader getShader(web_gl.RenderingContext gl, String id) {
