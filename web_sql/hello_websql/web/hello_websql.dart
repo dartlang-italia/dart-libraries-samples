c9e57305a0a28920887c0eddfbfba06e81839fc5
Claudio d'Angelis <claudiodangelis@gmail.com>
2014-08-02 14:42:04 +0200

    Added some comments to web_sql example

--- web_sql/hello_websql/web/hello_websql.dart
+++ web_sql/hello_websql/web/hello_websql.dart
@@ -3,30 +3,35 @@ import 'dart:web_sql';
 
 void main() {
 
-  // TODO: moar comments
+  // Database data
   SqlDatabase sql;
   String dbName = "myDb";
   String displayName = "My Database";
   int estimatedSize = 4 * 1024 * 1024;
   String version = "1";
 
+  // DOM Elements
   ParagraphElement pMessage = querySelector('#message');
   InputElement inputItem = querySelector('#inputItem');
   ButtonElement btnAddItem = querySelector('#btnAddItem');
   ButtonElement btnClearTable = querySelector('#btnClearDb');
   DivElement divResults = querySelector('#results');
 
-  // Checking for browser support
+  // Checks for browser support
   if (!SqlDatabase.supported) {
     pMessage.text = "It looks like WebSQL is not supported on this browser :-(";
     btnAddItem.disabled = true;
     inputItem.disabled = true;
   }
 
+  // Creates the database if it does not exist, then opens it
   sql = window.openDatabase(dbName, version, displayName, estimatedSize);
 
+  // Starts the transaction
   sql.transaction((tx) {
     print("Creating table if not exists");
+
+    // Executes a SQL query
     tx.executeSql('CREATE TABLE IF NOT EXISTS things (value unique)', []);
 
     tx.executeSql('SELECT * FROM things', [], (tx, SqlResultSet resultSet) {
