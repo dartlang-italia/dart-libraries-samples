19198e3c71e760f0de70b3884a551202f68602aa
Claudio d'Angelis <claudiodangelis@gmail.com>
2014-08-21 19:14:04 +0200

    Moar comments on deferred example

--- async/deferred_library/bin/deferred_library.dart
+++ async/deferred_library/bin/deferred_library.dart
@@ -1,20 +1,23 @@
 /*
- * Why use deferred libraries? 
+ * Why use deferred libraries?
  * see http://blog.sethladd.com/2013/04/lazy-load-libraries-in-dart.html
  */
 
 import 'dart:async';
 
+// Here we declare that loading of these two libraries is deferred
 @lazyItalian
 import '../lib/foo_italian.dart' as foo_italian;
 
 @lazyKorean
 import '../lib/foo_korean.dart' as foo_korean;
 
+
 const lazyItalian = const DeferredLibrary('foo_italian');
 const lazyKorean = const DeferredLibrary('foo_korean');
 
 void main() {
+  // Libraries are actually loaded
   lazyItalian.load().then(onItalianLoaded);
   lazyKorean.load().then(onKoreanLoaded);
 }
