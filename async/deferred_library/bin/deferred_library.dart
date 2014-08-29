28bfb544a99b05409d5c02ec2e9dc99a4c931a95
Claudio d'Angelis <claudiodangelis@gmail.com>
2014-08-29 15:40:01 +0200

    Clean deferred

--- async/deferred_library/bin/deferred_library.dart
+++ async/deferred_library/bin/deferred_library.dart
@@ -7,10 +7,10 @@ import 'dart:async';
 
 // Here we declare that loading of these two libraries is deferred
 @lazyItalian
-import '../lib/foo_italian.dart' as foo_italian;
+import 'package:deferred_library_sample/foo_italian.dart'  as foo_italian;
 
 @lazyKorean
-import '../lib/foo_korean.dart' as foo_korean;
+import 'package:deferred_library_sample/foo_korean.dart'  as foo_korean;
 
 
 const lazyItalian = const DeferredLibrary('foo_italian');
