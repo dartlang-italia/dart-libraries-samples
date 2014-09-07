6a75f49299c24e8e180ef658449945e04cf7c86f
Claudio d'Angelis <claudiodangelis@gmail.com>
2014-09-07 19:28:09 +0200

    More comments to websocket sample

--- html/web_sockets/web/web_sockets.dart
+++ html/web_sockets/web/web_sockets.dart
@@ -1,27 +1,35 @@
 import 'dart:html';
 
 void main() {
-  // TODO: add comments
+  // DOM elements selection
   TextAreaElement textarea = querySelector("#log");
   InputElement input = querySelector("#input");
   ButtonElement sendButton = querySelector("#btn");
   ButtonElement closeButton = querySelector("#close");
+
+  // Creates a new websocket connection
   WebSocket ws = new WebSocket("ws://echo.websocket.org");
+
+  // Registers a callback to the onOpen event
   ws.onOpen.listen((e) {
     textarea.appendText("[INFO] Connected\n");
 
+    // Registers a callback to the onMessage event
     ws.onMessage.listen((MessageEvent e) {
       textarea.appendText("[RECEIVED] ${e.data}\n");
     });
   });
 
+  // Registers a callback to the onClose event
   ws.onClose.listen((e) {
     textarea.appendText("[INFO] Disconnected\n");
   });
 
   sendButton.onClick.listen((e) {
+    // If the readyState is OPEN...
     if (ws.readyState == WebSocket.OPEN) {
       if (input.value.trim().isNotEmpty) {
+        // ... sends a message
         ws.send(input.value.trim());
         textarea.appendText("[SENT] ${input.value.trim()}\n");
         input.value = '';
@@ -32,7 +40,9 @@ void main() {
   });
 
   closeButton.onClick.listen((e) {
+    // If connection is not closed or closing...
     if (ws.readyState != WebSocket.CLOSED && ws.readyState != WebSocket.CLOSING) {
+      // closes connection
       ws.close();
     } else {
       textarea.appendText("[INFO] Connection already closed or closing\n");
