501672871d66353c3eaf9c1786ea85a9b0b8a712
Claudio d'Angelis <claudiodangelis@gmail.com>
2014-09-07 19:30:53 +0200

    Added comments to user_input.dart

--- io/user_input/bin/user_input.dart
+++ io/user_input/bin/user_input.dart
@@ -2,12 +2,15 @@ import 'dart:io';
 
 void main() {
   print("What's your name?");
+  // Reads a line from the standard input. The suffix `Sync' indicates that
+  // the methods is not asynchronous and it will block the execution
   String name = stdin.readLineSync();
   print("Hi $name!");
   print("What's your password?");
   print("it won't be displayed if you are running this script in a terminal");
+  // Disables echoing of text input
   stdin.echoMode = false;
   String password = stdin.readLineSync();
   stdin.echoMode = true;
-  // TODO: comments and other samples
+  // TODO: other samples
 }
